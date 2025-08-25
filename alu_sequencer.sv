e64b6af7058067d5f14d302375010558909ebe6e
vjarunaabirami <113103722+vjarunaabirami@users.noreply.github.com>
2025-08-25 22:02:43 +0530

    Create alu_sequencer.sv

---
+++ alu_sequencer.sv
@@ -0,0 +1,10 @@
+
+class alu_sequencer extends uvm_sequencer#(alu_sequence_item);
+
+  `uvm_component_utils(alu_sequencer)
+
+  function new(string name, uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
